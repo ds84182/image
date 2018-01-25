d3030d91943fa7b9fe61dfa658605bfc0a3d5ac6
Dwayne Slater <ds84182@gmail.com>
2018-01-24 21:14:45 -0500

    Use normal shifting in _shiftR and _shiftL

--- lib/src/util/bit_operators.dart
+++ lib/src/util/bit_operators.dart
@@ -4,20 +4,15 @@ int _shiftR(int v, int n) {
   // dart2js can't handle binary operations on negative numbers, so
   // until that issue is fixed (issues 16506, 1533), we'll have to do it
   // the slow way.
-  return (v / _SHIFT_BITS[n]).floor();
+  return (v >> n).toSigned(32);
 }
 
 int _shiftL(int v, int n) {
   // dart2js can't handle binary operations on negative numbers, so
   // until that issue is fixed (issues 16506, 1533), we'll have to do it
   // the slow way.
-  return (v * _SHIFT_BITS[n]);
+  return (v << n).toSigned(32);
 }
-
-const List<int> _SHIFT_BITS = const [
-  1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
-  32768, 65536];
-
 
 /**
  * Binary conversion of a uint8 to an int8.  This is equivalent in C to
